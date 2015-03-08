d403f0898bf2a6269e48e3748fc65fa0d4cbec36
unknown <kristoffer.berge@gmail.com>
2015-03-08 23:13:36 +0100

    Added script for setting don't destroy on load

---
+++ dontDestroyOnLoad.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class dontDestroyOnLoad : MonoBehaviour {
+    public GameObject canvas;
+    public GameObject persistant;
+    public GameObject player;
+
+	void Awake () {
+        DontDestroyOnLoad(canvas);
+        DontDestroyOnLoad(persistant);
+        DontDestroyOnLoad(player);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}

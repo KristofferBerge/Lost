7bd410229078c7e50c7c0199496f7d5c71e303b3
unknown <kristoffer.berge@gmail.com>
2015-03-08 19:51:39 +0100

    changed text-fading from animator to script

--- postMissionText.cs
+++ postMissionText.cs
@@ -5,9 +5,7 @@ using UnityEngine.UI;
 public class postMissionText : MonoBehaviour {
 
 	public Text missionText;
-	private Animator fadeAnim;
-
-	private string test = "Dette er en liten test for å se hvor fort og hvor langt denne mission.boksen kan skrive. Det skrives en bokstav hvert 0.01-sekund.";
+	public float printSpeed;
 
 	//printText uses WaitForSeconds and needs to be started as Coroutine
 	public void printMissionText(string txt){
@@ -18,24 +16,23 @@ public class postMissionText : MonoBehaviour {
 		char[] t = txt.ToCharArray ();
 		string toPrint = "";
 
-		//Adds one letter from array, posts text and waits for 0.01s
+		//Adds one letter from array, posts text and waits
 		for (int i = 0; i < t.Length; i++) {
 			toPrint += t[i];
 			missionText.text = toPrint;
-			yield return new WaitForSeconds(0.01f);
+			yield return new WaitForSeconds(printSpeed);
 		}
 		//Displays complete text for 5s
 		yield return new WaitForSeconds (5);
-		fadeAnim.SetTrigger ("fadeOut");
-		//waits for fade out animation to finish, then empties textboc before alpha is reset by animation.
-		yield return new WaitForSeconds(2);
+		missionText.CrossFadeAlpha (0, 0.5f, false);
+		yield return new WaitForSeconds (2);
+		//Empties textbox in canvas before setting alpha back to 1
 		missionText.text = "";
+		missionText.CrossFadeAlpha (1,1,false);
 	}
 
 	
 	void Start () {
-		fadeAnim = GetComponentInChildren<Animator> ();
-		printMissionText (test);
 	}
 	
 	// Update is called once per frame

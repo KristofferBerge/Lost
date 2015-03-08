15fcc0b74957f74c2b77e5d303a244a8bf42461d
unknown <kristoffer.berge@gmail.com>
2015-03-08 23:14:26 +0100

    added script for setting player position when loading new level

---
+++ setPlayerTransform.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class setPlayerTransform : MonoBehaviour {
+    private Vector3 spawn;
+    public GameObject indicator;
+    private GameObject player;
+	// Use this for initialization
+	void Start () {
+        player = GameObject.Find("First Person Controller");
+        player.transform.localScale = new Vector3(1f, 1f, 1f);
+        spawn = new Vector3(indicator.transform.position.x,indicator.transform.position.y,indicator.transform.position.z);
+        player.transform.position = spawn;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}

5be4d6853904daa3e63d49d924315fb6061d9063
unknown <kristoffer.berge@gmail.com>
2015-03-08 22:27:06 +0100

    commenting

--- rayCast.cs
+++ rayCast.cs
@@ -9,18 +9,24 @@ public class rayCast : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
+
+        //Finding and setting center point on screen to match crosshair
         x = Screen.width/2;
         y = Screen.height/2;
+        //Locking mouse pointer
         Cursor.lockState = CursorLockMode.Locked;
 	}
 	
 	// Update is called once per frame
 	void Update () {
+
        Ray ray = Camera.main.ScreenPointToRay(new Vector3(x,y));
        RaycastHit hit;
 
+        //Player tries to interact with object
         if (Input.GetButton("Fire1")){
             if (Physics.Raycast(ray, out hit, 10)) {
+                    //If object can be picked up
                     if (hit.collider.tag == "pickup")
                     {
                         Debug.Log("I CAN HAZ");
@@ -34,6 +40,7 @@ public class rayCast : MonoBehaviour {
 
         if (Physics.Raycast(ray, out hit, 10)) {
             if (hit.collider.tag == "pickup") {
+                //If ray hits object that can be picked up
                 disp.text = "Right click to pick up";
             }
             else {

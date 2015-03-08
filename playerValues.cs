fbdb8040dd8cfdf67860b4e700ac365f13acefd7
unknown <kristoffer.berge@gmail.com>
2015-03-08 22:15:34 +0100

    Made script to hold player values like hunger. Should make own script for decreasing hunger/health

---
+++ playerValues.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class playerValues : MonoBehaviour {
+    private float hunger;
+    private float health;
+    private float drugs;
+    public int hungerDeclineRate;
+    public bool hungry = true;
+
+    public GameObject uiScript;
+    private uiUpdate update;
+
+    private void initHungerDecline() {
+        StartCoroutine (hungerDecline());
+    }
+
+    IEnumerator hungerDecline() {
+        while (hungry){
+            yield return new WaitForSeconds(1);
+            uiScript.GetComponent<uiUpdate>().setCurrentHunger(1);
+        }
+    }
+    void awake() { 
+        //update = uiScript.GetComponent<uiUpdate>();
+    }
+	// Use this for initialization
+	void Start () {
+        initHungerDecline();
+	}
+
+	// Update is called once per frame
+	void Update () {
+	}
+}

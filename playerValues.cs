73feb36a6341082d08754018a5091a36d20de184
unknown <kristoffer.berge@gmail.com>
2015-03-13 16:20:14 +0100

    changing logic for reducing health

--- playerValues.cs
+++ playerValues.cs
@@ -2,34 +2,24 @@
 using System.Collections;
 
 public class playerValues : MonoBehaviour {
-    private float hunger;
+    public float hungerDeclineRate;
     private float health;
     private float drugs;
-    public int hungerDeclineRate;
     public bool hungry = true;
 
     public GameObject uiScript;
     private uiUpdate update;
 
-    private void initHungerDecline() {
-        StartCoroutine (hungerDecline());
-    }
-
-    IEnumerator hungerDecline() {
-        while (hungry){
-            yield return new WaitForSeconds(1);
-            uiScript.GetComponent<uiUpdate>().setCurrentHunger(1);
-        }
-    }
     void awake() { 
         
     }
 	// Use this for initialization
 	void Start () {
-        initHungerDecline();
 	}
 
 	// Update is called once per frame
 	void Update () {
+        uiScript.GetComponent<uiUpdate>().setCurrentHunger(hungerDeclineRate*Time.deltaTime);
+
 	}
 }

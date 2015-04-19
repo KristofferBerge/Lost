688a6ad1f273fc756a423a9f8b7557655e7a6fbb
kristofferberge <kristoffer.berge@gmail.com>
2015-04-19 17:41:49 +0200

    Added script for playing music in bunker

---
+++ MusicPlayer.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MusicPlayer : MonoBehaviour {
+    public AudioSource lydkilde;
+	// Use this for initialization
+	void Start (){
+        lydkilde.Play();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}

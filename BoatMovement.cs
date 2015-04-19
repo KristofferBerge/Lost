fec94459aa1c32e0eeb6978a17e27c3fa2fab2a6
kristofferberge <kristoffer.berge@gmail.com>
2015-04-19 17:41:23 +0200

    added script for controlling boat

---
+++ BoatMovement.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BoatMovement : MonoBehaviour {
+    public int travelDistance;
+    public float speed;
+    private Vector3 startPos;
+    private Vector3 toPos;
+
+    void OnTriggerEnter(Collider other) {
+        Debug.Log("trigger enter");
+        startPos = transform.position;
+        toPos = new Vector3(startPos.x + travelDistance, startPos.y, startPos.z);
+        StartCoroutine(moveBoat());
+    }
+    private IEnumerator moveBoat() { 
+        while(Vector3.Distance(toPos,transform.position) > 1){
+            transform.position = new Vector3(transform.position.x + speed, transform.position.y, transform.position.z);
+            yield return new WaitForEndOfFrame();
+        }
+    }
+}
